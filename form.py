c503af3b82b57b06e4ab9f62a10f53c988e90a24
Bartosz Obstawski <115159235+bobstawski@users.noreply.github.com>
2023-12-06 19:43:29 +0100

    Add files via upload

---
+++ form.py
@@ -0,0 +1,88 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+endpoint = 'https://lab2-puwc-obstawski.cognitiveservices.azure.com/'
+key = '2245007b86a54426af68f800df81c267'
+
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+with open('zdj.png', "rb") as f:
+    poller = document_analysis_client.begin_analyze_document(
+        "prebuilt-layout", document=f
+    )
+result = poller.result()
+
+for idx, style in enumerate(result.styles):
+    print(
+        "Document contains {} content".format(
+            "handwritten" if style.is_handwritten else "no handwritten"
+        )
+    )
+
+for page in result.pages:
+    print("----Analyzing layout from page #{}----".format(page.page_number))
+    print(
+        "Page has width: {} and height: {}, measured with unit: {}".format(
+            page.width, page.height, page.unit
+        )
+    )
+
+    for line_idx, line in enumerate(page.lines):
+        words = line.get_words()
+        print(
+            "...Line # {} has word count {} and text '{}' within bounding polygon '{}'".format(
+                line_idx,
+                len(words),
+                line.content,
+                line.polygon,
+            )
+        )
+
+        for word in words:
+            print(
+                "......Word '{}' has a confidence of {}".format(
+                    word.content, word.confidence
+                )
+            )
+
+    for selection_mark in page.selection_marks:
+        print(
+            "...Selection mark is '{}' within bounding polygon '{}' and has a confidence of {}".format(
+                selection_mark.state,
+                selection_mark.polygon,
+                selection_mark.confidence,
+            )
+        )
+
+for table_idx, table in enumerate(result.tables):
+    print(
+        "Table # {} has {} rows and {} columns".format(
+            table_idx, table.row_count, table.column_count
+        )
+    )
+    for region in table.bounding_regions:
+        print(
+            "Table # {} location on page: {} is {}".format(
+                table_idx,
+                region.page_number,
+                region.polygon,
+            )
+        )
+    for cell in table.cells:
+        print(
+            "...Cell[{}][{}] has content '{}'".format(
+                cell.row_index,
+                cell.column_index,
+                cell.content,
+            )
+        )
+        for region in cell.bounding_regions:
+            print(
+                "...content on page {} is within bounding polygon '{}'".format(
+                    region.page_number,
+                    region.polygon,
+                )
+            )
+
+print("----------------------------------------")
