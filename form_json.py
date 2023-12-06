b3e4084b4668f475d7e26025f4dd6d3cb6a0e36a
Bartosz Obstawski <115159235+bobstawski@users.noreply.github.com>
2023-12-06 21:36:18 +0100

    Update form_json.py

--- form_json.py
+++ form_json.py
@@ -3,7 +3,7 @@ from azure.ai.formrecognizer import DocumentAnalysisClient
 import json
 
 endpoint = 'https://lab2-puwc-obstawski.cognitiveservices.azure.com/'
-key = '2245007b86a54426af68f800df81c267'
+key = 'confidetial'
 
 document_analysis_client = DocumentAnalysisClient(
     endpoint=endpoint, credential=AzureKeyCredential(key)
@@ -15,16 +15,13 @@ with open('zdj.png', "rb") as f:
     )
 result = poller.result()
 
-# Zapis wyników analizy do pliku JSON
 with open('analysis_results_formrecognizer.json', 'w') as output_file:
     output_file.write(str(result))
 
-# Wydobywanie tekstu z dokumentu
 text_data = []
 for page in result.pages:
     for line in page.lines:
         text_data.append(line.content)
 
-# Zapis wydobytego tekstu do pliku JSON
 with open('text_results.json', 'w') as text_output:
     json.dump(text_data, text_output, indent=4)
