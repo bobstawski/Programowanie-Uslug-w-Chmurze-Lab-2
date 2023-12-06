c36b7efacb6d1a058961dda4492b9e6407cec297
Bartosz Obstawski <115159235+bobstawski@users.noreply.github.com>
2023-12-06 21:36:39 +0100

    Update qna_auth.py

--- qna_auth.py
+++ qna_auth.py
@@ -2,11 +2,9 @@ import os
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.questionanswering.authoring import AuthoringClient
 
-# get service secrets
 endpoint = 'https://lab2-puwc-bo.cognitiveservices.azure.com/'
-key = 'bc950c4fb0014025b22c8d09df706a2b'
+key = 'confidential'
 
-# create client
 client = AuthoringClient(endpoint, AzureKeyCredential(key))
 
 project_name = "IssacNewton"
@@ -25,7 +23,6 @@ update_sources_poller = client.begin_update_sources(
 )
 update_sources_poller.result()
 
-# list sources
 print("list project sources")
 sources = client.list_sources(
     project_name=project_name
